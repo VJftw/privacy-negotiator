8357555a8f90bf9271aff15ae6acb6aa07c5a37b
VJ Patel <vj@vjpatel.me>
2017-04-10 22:34:56 +0100

    [worker] Added minimal Silex framework with amqplib

---
+++ worker/priv_neg/src/app.php
@@ -0,0 +1,14 @@
+<?php
+
+use Silex\Application;
+use Silex\Provider\AssetServiceProvider;
+use Silex\Provider\TwigServiceProvider;
+use Silex\Provider\ServiceControllerServiceProvider;
+use Silex\Provider\HttpFragmentServiceProvider;
+
+$app = new Application();
+$app->register(new ServiceControllerServiceProvider());
+$app->register(new HttpFragmentServiceProvider());
+
+
+return $app;
