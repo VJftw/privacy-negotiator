781492632b13fa5a41d164b5c473b98c0ee7b7fd
VJ Patel <vj@vjpatel.me>
2017-06-06 00:24:11 +0100

    [web_app] Sending facebook auth request to obtain authToken for API

--- web_app/priv-neg/src/app/index/index.component.ts
+++ web_app/priv-neg/src/app/index/index.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
 import { FacebookService, InitParams, LoginResponse } from 'ngx-facebook';
 
 @Component({
@@ -7,7 +9,10 @@ import { FacebookService, InitParams, LoginResponse } from 'ngx-facebook';
 })
 export class IndexComponent {
 
-  constructor(private fb: FacebookService) {
+  constructor(
+    private fb: FacebookService,
+    private http: Http
+  ) {
 
     let initParams: InitParams = {
       appId: '219608771883029',
@@ -19,10 +24,26 @@ export class IndexComponent {
   }
 
   loginWithFacebook(): void {
-
     this.fb.login()
-      .then((response: LoginResponse) => console.log(response))
+      .then((response: LoginResponse) => this.authenticateWithApi(response))
       .catch((error: any) => console.error(error));
+  }
 
+  authenticateWithApi(response: LoginResponse): void {
+    this.http.post(
+      "http://localhost/v1/auth",
+      JSON.stringify(response.authResponse),
+      {headers: new Headers({'Content-Type': 'application/json'})}
+    ).toPromise()
+    .then(res => console.log(res.json()))
+    .catch(this.handleError)
+    ;
+
+
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
   }
 }
