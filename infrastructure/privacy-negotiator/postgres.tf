3d4fb3ad8d54c29a03d836e9a14e10ed83ce19ad
VJ Patel <vj@vjpatel.me>
2017-06-09 23:36:19 +0100

    Updated security group for postgres

--- infrastructure/privacy-negotiator/postgres.tf
+++ infrastructure/privacy-negotiator/postgres.tf
@@ -1,24 +1,26 @@
 resource "aws_db_instance" "db" {
-  allocated_storage    = 10
-  storage_type         = "gp2"
-  engine               = "postgres"
-  instance_class       = "db.t2.micro"
-  name                 = "privneg_${var.environment}"
-  username             = "${var.postgres_user}"
-  password             = "${var.postgres_password}"
+  allocated_storage      = 10
+  storage_type           = "gp2"
+  engine                 = "postgres"
+  instance_class         = "db.t2.micro"
+  name                   = "privneg_${var.environment}"
+  username               = "${var.postgres_user}"
+  password               = "${var.postgres_password}"
   vpc_security_group_ids = ["${aws_security_group.db.id}"]
-  db_subnet_group_name = "${aws_db_subnet_group.db.id}"
+  db_subnet_group_name   = "${aws_db_subnet_group.db.id}"
+
   // skip_final_snapshot = true
   final_snapshot_identifier = "priv-neg-${var.environment}"
 }
 
 resource "aws_db_subnet_group" "db" {
-  name       = "privneg"
+  name = "privneg"
+
   subnet_ids = [
     "${data.aws_subnet.app_cluster.0.id}",
     "${data.aws_subnet.app_cluster.1.id}",
     "${data.aws_subnet.app_cluster.2.id}",
-    ]
+  ]
 
   tags {
     Name = "privneg-db-subnet-grp"
@@ -26,16 +28,14 @@ resource "aws_db_subnet_group" "db" {
 }
 
 resource "aws_security_group" "db" {
-
-  name        = "${var.environment}-db-sg"
-  vpc_id      = "${data.aws_vpc.app_cluster.id}"
+  name   = "${var.environment}-db-sg"
+  vpc_id = "${data.aws_vpc.app_cluster.id}"
 
   # MySQL access from the VPC
   ingress {
-    from_port   = 3306
-    to_port     = 3306
+    from_port   = 5432
+    to_port     = 5432
     protocol    = "tcp"
     cidr_blocks = ["10.0.0.0/16"]
   }
-
 }
