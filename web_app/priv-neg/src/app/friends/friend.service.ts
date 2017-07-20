ee6b8aab1aaadac5e06c7e09650367312511b56e
VJ Patel <vj@vjpatel.me>
2017-07-21 00:23:40 +0100

    [web_app] Fetching 500 friends from facebook api each time

--- web_app/priv-neg/src/app/friends/friend.service.ts
+++ web_app/priv-neg/src/app/friends/friend.service.ts
@@ -128,7 +128,7 @@ export class FriendService implements Channel {
 
 
   public updateFriends(offset = null): Promise<any> {
-    let uri = '/me/friends?fields=id,first_name,last_name,picture{url}&limit=500';
+    let uri = '/me/friends?fields=id,first_name,last_name,picture.type(large){url}&limit=500';
 
     if (offset) {
       uri += '&after=' + offset;
